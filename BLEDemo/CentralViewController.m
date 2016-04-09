bcb33edf934a3d490234c6efb752d790db2977b4
aaronCai <dcwang@foxmail.com>
2016-04-09 22:56:31 +0800

    增加外设和中心设备演示功能,包括通知,读数据和写数据

--- BLEDemo/CentralViewController.m
+++ BLEDemo/CentralViewController.m
@@ -8,11 +8,17 @@
 
 #import "CentralViewController.h"
 #import <CoreBluetooth/CoreBluetooth.h>
+#import "Header.h"
 
 @interface CentralViewController ()<CBCentralManagerDelegate,CBPeripheralDelegate>
 
+
 @property (nonatomic, strong) CBCentralManager *centralManager; //中心管理者
 @property (nonatomic, strong) NSMutableArray *peripherals; //连接的外围设备
+@property (nonatomic, strong) CBCharacteristic *curCharacter; //当前服务的特征
+@property (nonatomic, strong) CBPeripheral *curPeripherals;
+
+@property (weak, nonatomic) IBOutlet UITextView *logText;
 
 @end
 
@@ -28,6 +34,47 @@
     _peripherals = [NSMutableArray array];
 }
 
+- (void)writeToLogWithText:(NSString *)text{
+    
+    NSLog(@"%@",text);
+    self.logText.text = [NSString stringWithFormat:@"%@\n%@",self.logText.text,text];
+}
+
+#pragma mark - private method 
+
+- (IBAction)cancelDidClick:(UIButton *)sender {
+    
+    [self cancelNotify];
+}
+
+- (IBAction)confignotify:(UIButton *)sender {
+    
+    [self configNotify];
+}
+
+- (IBAction)sendData:(UIButton *)sender {
+    
+    if (_curCharacter != nil && _curPeripherals !=nil) {
+        [_curPeripherals writeValue:[@"hello,外设" dataUsingEncoding:NSUTF8StringEncoding] forCharacteristic:_curCharacter type:CBCharacteristicWriteWithResponse];
+        [self writeToLogWithText:@"写数据给外设"];
+    }
+}
+
+//取消订阅
+- (void)cancelNotify{
+    
+    CBPeripheral *peripheral = _peripherals.firstObject;
+    [peripheral setNotifyValue:NO forCharacteristic:_curCharacter];
+    [self writeToLogWithText:@"取消订阅特征通知"];
+}
+
+//订阅特征
+- (void)configNotify{
+    
+    CBPeripheral *peripheral = _peripherals.firstObject;
+    [peripheral setNotifyValue:YES forCharacteristic:_curCharacter];
+    [self writeToLogWithText:@"订阅特征通知"];
+}
 
 #pragma mark - CBCentralManagerDelegate
 
@@ -35,31 +82,32 @@
     
     switch (central.state) {
         case CBCentralManagerStatePoweredOn:
-            NSLog(@"中心设备已打开");
+            [self writeToLogWithText:@"中心设备已打开"];
             [central scanForPeripheralsWithServices:nil options:nil];
             break;
             
         default:
-            NSLog(@"此设备不支持BLE或未打开蓝牙功能");
+            [self writeToLogWithText:@"此设备不支持BLE或未打开蓝牙功能"];
             break;
     }
 }
 
 - (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *,id> *)advertisementData RSSI:(NSNumber *)RSSI{
-    NSLog(@"发现外围设备:%@",peripheral);
+    
+    [self writeToLogWithText:[NSString stringWithFormat:@"发现外围设备:%@",peripheral]];
 //    [_centralManager stopScan];
-    if ([peripheral.name hasPrefix:@"12"]) {
+    if ([peripheral.name hasPrefix:@"iP"]|| [peripheral.name hasPrefix:@"aaron"]) {
         if (![_peripherals containsObject:peripheral]) {
             [_peripherals addObject:peripheral];
         }
-        NSLog(@"开始连接外围设备--%@",peripheral);
+        [self writeToLogWithText:[NSString stringWithFormat:@"开始连接外围设备--%@",peripheral]];
         [_centralManager connectPeripheral:peripheral options:nil];
     }
 }
 
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral{
-    NSLog(@"连接设备成功;%@",peripheral);
-    
+
+    [self writeToLogWithText:@"连接设备成功"];
     peripheral.delegate = self;
     
     //外围设备开始寻找服务
@@ -69,57 +117,67 @@
 #pragma mark - CBPeripheralDelegate
 
 - (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error{
-//    NSLog(@"外围设备发现services\n");
     
+    [self writeToLogWithText:@"已发现可用服务"];
     for (CBService *service in peripheral.services) {
-//        NSLog(@"service:%@\n",service);
         //外围设备查找指定服务中的特征
         [peripheral discoverCharacteristics:nil forService:service];
     }
 }
 
 - (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(nonnull CBService *)service error:(nullable NSError *)error{
-//    NSLog(@"外围设备查找指定服务中的特征");
     
+    [self writeToLogWithText:@"已发现可用特征"];
     for (CBCharacteristic *characteristic in service.characteristics) {
-//        NSLog(@"%@--%@",service,characteristic);
         
         //情景一：读取
-        if (characteristic.properties & CBCharacteristicPropertyRead) {
-            [peripheral readValueForCharacteristic:characteristic];
-            if (characteristic.value) {
-                NSString *value=[[NSString alloc]initWithData:characteristic.value encoding:NSUTF8StringEncoding];
-                NSLog(@"读取到特征值：%@",value);
-            }
-        }
+//        if (characteristic.properties & CBCharacteristicPropertyRead) {
+//            if ([characteristic.UUID.UUIDString isEqualToString:kReadUUID]) {
+//                [peripheral readValueForCharacteristic:characteristic];
+//                if (characteristic.value) {
+//                    NSString *value=[[NSString alloc]initWithData:characteristic.value encoding:NSUTF8StringEncoding];
+//                    NSLog(@"读取到特征值：%@",value);
+//                }
+//            }
+//        }
 
         //情景二：通知
         if (characteristic.properties & CBCharacteristicPropertyNotify) {
-            [peripheral setNotifyValue:YES forCharacteristic:characteristic];
-            NSLog(@"更新前通知：%@",characteristic);
+            if ([characteristic.UUID.UUIDString isEqualToString:kNotifyUUID] || [characteristic.UUID.UUIDString isEqualToString:kWriteUUID]) {
+                _curCharacter = characteristic;
+                [peripheral setNotifyValue:YES forCharacteristic:characteristic];
+                [self writeToLogWithText:@"已订阅特征通知"];
+            }
         }
         
         //情景二：写数据
         if (characteristic.properties & CBCharacteristicPropertyWrite) {
-            [peripheral writeValue:[@"1234" dataUsingEncoding:NSUTF8StringEncoding] forCharacteristic:characteristic type:CBCharacteristicWriteWithResponse];
-            NSLog(@"写前：%@",characteristic);
+            if ([characteristic.UUID.UUIDString isEqualToString:kWriteUUID]) {
+                [peripheral writeValue:[@"hello,外设" dataUsingEncoding:NSUTF8StringEncoding] forCharacteristic:characteristic type:CBCharacteristicWriteWithResponse];
+                [self writeToLogWithText:@"写数据给外设"];
+                _curPeripherals = peripheral;
+                _curCharacter = characteristic;
+            }
         }
     }
 }
 
 - (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(nonnull CBCharacteristic *)characteristic error:(nullable NSError *)error{
-//    NSLog(@"收到特征更新通知...");
-    NSLog(@"更新后通知：%@",characteristic);
+    
+    NSString *value=[[NSString alloc]initWithData:characteristic.value encoding:NSUTF8StringEncoding];
+    [self writeToLogWithText:[NSString stringWithFormat:@"读取到更新后通知：%@",value]];
 }
 
 - (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(nonnull CBCharacteristic *)characteristic error:(nullable NSError *)error{
+    
     NSString *value=[[NSString alloc]initWithData:characteristic.value encoding:NSUTF8StringEncoding];
-    NSLog(@"读更新后的特征值：%@",value);
+    [self writeToLogWithText:[NSString stringWithFormat:@"读取特征值：%@",value]];
 }
 
 - (void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(nonnull CBCharacteristic *)characteristic error:(nullable NSError *)error{
+    
     NSString *value=[[NSString alloc]initWithData:characteristic.value encoding:NSUTF8StringEncoding];
-    NSLog(@"写更新后的特征值：%@",value);
+    [self writeToLogWithText:[NSString stringWithFormat:@"读取写更新特征值：%@",value]];
 }
 
 @end
--- BLEDemo/PeripheralViewController.m
+++ BLEDemo/PeripheralViewController.m
@@ -7,31 +7,156 @@
 //
 
 #import "PeripheralViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "Header.h"
 
-@interface PeripheralViewController ()
+
+@interface PeripheralViewController ()<CBPeripheralManagerDelegate>
+@property (strong, nonatomic) CBPeripheralManager *peripheralManager;  //外围设备管理器
+@property (strong, nonatomic) NSMutableArray *centralList;             //订阅此外围设备特征的中心设备
+@property (strong, nonatomic) CBMutableCharacteristic *characteristic; //特征
+@property (strong, nonatomic) CBMutableService *service;               //服务
+
+@property (weak, nonatomic) IBOutlet UITextView *logText;
 
 @end
 
 @implementation PeripheralViewController
+
+#pragma mark - life cycle
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - acton
+
+- (IBAction)start:(UIButton *)sender {
+    
+    NSLog(@"启动外设");
+    
+    if (_peripheralManager == nil) {
+        _peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil];
+        _centralList = [NSMutableArray array];
+    }
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)update:(UIButton *)sender {
+    
+    NSLog(@"更新特征值");
+    [self updataCharacteristic];
 }
-*/
+
+- (void)writeToLogWithText:(NSString *)text{
+    
+    NSLog(@"%@",text);
+    self.logText.text = [NSString stringWithFormat:@"%@\n%@",self.logText.text,text];
+}
+
+#pragma mark - CBPeripheralManagerDelegate
+
+- (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral{
+    
+
+    switch (peripheral.state) {
+        case CBPeripheralManagerStatePoweredOn:
+            [self writeToLogWithText:@"外围设备BLE已打开"];
+            
+            [self setupService];
+            break;
+            
+        default:
+            [self writeToLogWithText:@"此设备不支持BLE或未打开蓝牙功能，无法作为外围设备"];
+            break;
+    }
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral didAddService:(CBService *)service error:(nullable NSError *)error{
+    
+    NSDictionary *dict = @{CBAdvertisementDataLocalNameKey:kPeripheralName};
+    [_peripheralManager startAdvertising:dict];
+    [self writeToLogWithText:@"向外围设备添加了服务"];
+}
+
+- (void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheral error:(nullable NSError *)error{
+ 
+    [self writeToLogWithText:@"启动广播..."];
+}
+
+
+//订阅特征
+- (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didSubscribeToCharacteristic:(CBCharacteristic *)characteristic{
+    
+    [self writeToLogWithText:[NSString stringWithFormat:@"中心设备：%@ 已订阅特征：%@.",central.identifier.UUIDString,characteristic.UUID]];
+    if (![_centralList containsObject:central]) {
+        [_centralList addObject:central];
+    }
+    
+}
+
+//取消订阅
+- (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didUnsubscribeFromCharacteristic:(CBCharacteristic *)characteristic{
+ 
+    [self writeToLogWithText:[NSString stringWithFormat:@"中心设备：%@ 已取消订阅特征：%@.",central.identifier.UUIDString,characteristic.UUID]];
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveReadRequest:(CBATTRequest *)request{
+    
+    [self writeToLogWithText:@"中心设备读外设数据"];
+    [self writeToLogWithText:[[NSString alloc] initWithData:request.value encoding:NSUTF8StringEncoding]];
+}
+
+-(void)peripheralManager:(CBPeripheralManager *)peripheral willRestoreState:(NSDictionary *)dict{
+    
+    [self writeToLogWithText:@"willRestoreState"];
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveWriteRequests:(NSArray<CBATTRequest *> *)requests{
+    
+    [self writeToLogWithText:@"收到中心写来的数据"];
+    CBATTRequest *request = requests.lastObject;
+    [self writeToLogWithText:[[NSString alloc] initWithData:request.value encoding:NSUTF8StringEncoding]];
+    [peripheral respondToRequest:request withResult:CBATTErrorSuccess];
+    
+}
+
+
+#pragma mark - private method
+
+//创建服务,特征并添加服务到外围设备
+- (void)setupService{
+    
+    //可通知的特征
+//    CBUUID *characteristicUUID = [CBUUID UUIDWithString:kNotifyUUID];
+//    _characteristic = [[CBMutableCharacteristic alloc] initWithType:characteristicUUID properties:CBCharacteristicPropertyNotify value:nil permissions:CBAttributePermissionsReadable];
+    
+    //可读写的特征
+    CBUUID *UUID2 = [CBUUID UUIDWithString:kWriteUUID];
+    _characteristic = [[CBMutableCharacteristic alloc] initWithType:UUID2 properties:CBCharacteristicPropertyWrite|CBCharacteristicPropertyNotify value:nil permissions:CBAttributePermissionsWriteEncryptionRequired];
+//
+    //只读的特征
+//    CBUUID *UUID3 = [CBUUID UUIDWithString:kReadUUID];
+//    NSData *characteristicValue = [@"aaron才" dataUsingEncoding:NSUTF8StringEncoding];
+//    _characteristic = [[CBMutableCharacteristic alloc] initWithType:UUID3 properties:CBCharacteristicPropertyRead value:characteristicValue permissions:CBAttributePermissionsReadable];
+    
+    CBUUID *serviceUUID = [CBUUID UUIDWithString:kServiceUUID];
+    _service = [[CBMutableService alloc] initWithType:serviceUUID primary:YES];
+    [_service setCharacteristics:@[_characteristic]];
+    
+    [_peripheralManager addService:_service];
+    
+    
+}
+
+- (void)updataCharacteristic{
+    
+    NSString *valueStr = [NSString stringWithFormat:@"%@: %@",kPeripheralName,[NSDate date]];
+    NSData *data = [valueStr dataUsingEncoding:NSUTF8StringEncoding];
+    [_peripheralManager updateValue:data forCharacteristic:_characteristic onSubscribedCentrals:nil];
+    
+    [self writeToLogWithText:[NSString stringWithFormat:@"更新特征值:%@",valueStr]];
+}
 
 @end
